edd2914a3f95b8fe3fca5ff486a98244c3eea6df
Catherine Zhou <cz284@cornell.edu>
2017-11-15 21:21:49 -0500

    Change command to use position from models

--- command.mli
+++ command.mli
@@ -1,9 +1,19 @@
 (* [command] represents a command input given by a change in the GUI made
  * by a player. *)
-type command = Move of (int * int) * (int * int) |
-               Take of (int * int) * (int * int) |
-               ShortCastle | LongCastle | Promotion of (string * int) |
-               Captured | Quit | Invalid
+type command =
+  | Move of Models.position * Models.position
+  | Take of Models.position * Models.position
+  | ShortCastle
+  | LongCastle
+  | Promotion of Models.position * Models.piece
+  | Captured
+  | Quit
+  | Invalid
+
+
+
+
+
 
 (* [parse str] is the command that represents player input [str].
  * requires: [str] is one of the commands forms described in the
