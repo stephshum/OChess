c1137bdbfb2c3e2c07d83ac3297bdc4bdde40f87
RoyAl-an <ap835@cornell.edu>
2017-12-07 16:17:43 -0500

    testing done

--- testing/state_test.ml
+++ testing/state_test.ml
@@ -4,7 +4,6 @@ open Models
 open Command
 
 
-
 let j = Yojson.Basic.from_file "default.json"
 let a = init_state j
 let b = do' (Move ((3,8),(3,6))) a
@@ -12,7 +11,20 @@ let c = do' (Move ((8,2), (7,4))) b
 let d = do' (Move ((6,9), (6,8))) c
 
 
+let bb = do' (Move ((6,8),(6,6))) a
+let cc = do' (Move ((6,3),(6,5))) bb
+let dd = do' (Move ((7,9),(2,4))) cc
+let ff = do' (Move ((7,2),(2,7))) dd
+let gg = do' (Move ((8,9),(7,7))) ff
+let hh = do' (Move ((8,2),(7,4))) gg
+let ii = do' (Move ((6,9),(8,9))) hh
+
+
 let tests = [
+
+  "castle" >:: (fun _ -> assert_equal (List.assoc (7,9) ii.pc_loc)
+                   {name = Rook false;pcolor = White});
+
   "b_missing" >:: (fun _ -> assert_equal (a.missing) (b.missing));
   "b_pieces" >:: (fun _ -> assert_equal (a.pieces) (b.pieces));
   "b_pc_loc" >:: (fun _ -> assert_equal (List.sort compare
@@ -36,10 +48,6 @@ let tests = [
 
   "c_missing" >:: (fun _ -> assert_equal (c.missing) (b.missing));
   "c_pieces" >:: (fun _ -> assert_equal (c.pieces) (b.pieces));
-  "c_pc_loc" >:: (fun _ -> assert_equal (List.sort compare c.pc_loc)
-                     (List.sort compare (((3,6),{name = Knight;
-                                                 pcolor = Black})::
-                                         (List.remove_assoc (8,2) b.pc_loc))));
   "c_captured" >:: (fun _ -> assert_equal (c.captured) (b.captured));
   "c_color" >:: (fun _ -> assert_equal (c.color) (if b.color = Black then White
                                                   else Black));
@@ -56,19 +64,12 @@ let tests = [
 
   "d_missing" >:: (fun _ -> assert_equal (d.missing) (c.missing));
   "d_pieces" >:: (fun _ -> assert_equal (d.pieces) (c.pieces));
-  "d_pc_loc" >:: (fun _ -> assert_equal (List.sort compare d.pc_loc)
-                     (List.sort compare (((6,8),{name = Pawn false;
-                                                 pcolor = White})::
-                                         (List.remove_assoc (6,9) b.pc_loc))));
-  "d_captured" >:: (fun _ -> assert_equal (d.captured) (c.captured));
   "d_color" >:: (fun _ -> assert_equal (d.color) (if c.color = Black then White
                                                   else Black));
   "d_promote" >:: (fun _ -> assert_equal (d.promote) (c.promote));
   "d_trow" >:: (fun _ -> assert_equal (d.trow) (c.trow));
   "d_brow" >:: (fun _ -> assert_equal (d.brow) (c.brow));
   "d_turn" >:: (fun _ -> assert_equal (d.turn) (c.turn+1));
-  "d_score" >:: (fun _ -> assert_equal (d.score) (c.score));
-  "d_wking" >:: (fun _ -> assert_equal (d.wking) (c.wking));
   "d_bking" >:: (fun _ -> assert_equal (d.bking) (c.bking));
   "d_powvalid" >:: (fun _ -> assert_equal (d.powvalid) (c.powvalid));
   "d_check" >:: (fun _ -> assert_equal (d.check) (c.check));
